a98c555b9a75496dcc2b9cf0e551fa52ba37a345
httg9 <alin29@buffalo.edu>
2019-10-16 20:23:56 -0400

    Made Bullet, Enemy and Player classes. This links to User Story "Player Shooting Functionality" Issue #23 and Task "Player Shooting Projectiles" Issue #17 and User Story "Enemy Ship Spawn Functionality" Issue #24 and Task "Enemy Ship Display" Issue #25

--- View/GameplayView.py
+++ View/GameplayView.py
@@ -3,25 +3,21 @@ import random
 import pygame
 from View.ParentView import View, GameSprite
 
-# CLass for defining the gameplay portion
+
+# Class for defining the gameplay portion
 class GameplayView(View):
     def __init__(self):
         super(GameplayView, self).__init__()
         self.name = "Gameplay"
         self.bg = pygame.image.load('Menu/Blank_Page.png')
+        # creates player class
+        self.player = Player(GameSprite(310, 600, 64, 64, self.BasicShipFrames, 0))
 
-        self.shipSpeed = 4
-        self.fireRate = .5
         self.bulletTimer = 0
-        self.player = GameSprite(310, 600, 64, 64, self.BasicShipFrames, 0, self.shipSpeed)
-
-        self.bulletSpeed = 5
-        self.currentBullet = [pygame.image.load('Projectiles/Small_Basic_Bullet.png')]
-        self.bulletSound = pygame.mixer.Sound('Projectiles/Basic_Bullet.wav')
         # List of each bullet
-        self.bullet_list = pygame.sprite.Group()
+        self.bulletList = pygame.sprite.Group()
 
-        self.EnemyImpalerLvl1 = GameSprite(random.randint(0, 500), -96, 96, 96, self.EnemyImpalerShipLvl1Frames, 0, 3)
+        self.EnemyImpalerLvl1 = Enemy(GameSprite(random.randint(0, 500), -96, 96, 96, self.EnemyImpalerShipLvl1Frames, 0))
     # Draws background and Player ship
 
     def draw(self, mouse, dt):
@@ -29,34 +25,65 @@ class GameplayView(View):
         self.bulletTimer -= dt
         # repeatedly draws the screen, must provide: (mouse position, milliseconds since last frame)
         self.screen.blit(self.bg, (0, 0))
-        self.player.update(self.screen, dt)
-        for bullet in self.bullet_list:
-            bullet.ycor -= bullet.yspeed
-            if bullet.ycor < -10:
-                self.bullet_list.remove(bullet)
-        self.bullet_list.update(self.screen, dt)
         self.move_enemy_ship_down(self.EnemyImpalerLvl1)
-        self.EnemyImpalerLvl1.update(self.screen, dt)
+        self.player.gameSprite.update(self.screen, dt)
+        for bullet in self.bulletList:
+            bullet.ycor -= self.player.bulletSpeed
+            if bullet.ycor < -10:
+                self.bulletList.remove(bullet)
+        self.EnemyImpalerLvl1.gameSprite.update(self.screen, dt)
+        self.bulletList.update(self.screen, dt)
         pygame.display.update()
 
     def key_event(self, key):
         if key[pygame.K_DOWN] or key[pygame.K_s]:
-            if self.player.ycor + self.player.yspeed < self.windowHeight - 70:
-                self.player.ycor += self.player.yspeed
+            if self.player.gameSprite.ycor + self.player.playerSpeed < self.windowHeight - 70:
+                self.player.gameSprite.ycor += self.player.playerSpeed
         if key[pygame.K_UP] or key[pygame.K_w]:
-            if self.player.ycor - self.player.yspeed > 0:
-                self.player.ycor -= self.player.yspeed
+            if self.player.gameSprite.ycor - self.player.playerSpeed > 0:
+                self.player.gameSprite.ycor -= self.player.playerSpeed
         if key[pygame.K_LEFT] or key[pygame.K_a]:
-            if self.player.xcor + self.player.xspeed > 0:
-                self.player.xcor -= self.player.xspeed
+            if self.player.gameSprite.xcor + self.player.playerSpeed > 0:
+                self.player.gameSprite.xcor -= self.player.playerSpeed
         if key[pygame.K_RIGHT] or key[pygame.K_d]:
-            if self.player.xcor + self.player.xspeed < self.windowWidth - 60:
-                self.player.xcor += self.player.xspeed
+            if self.player.gameSprite.xcor + self.player.playerSpeed < self.windowWidth - 60:
+                self.player.gameSprite.xcor += self.player.playerSpeed
         if key[pygame.K_SPACE]:
             if self.bulletTimer <= 0:
-                self.bulletTimer = self.fireRate
-                self.bulletSound.play()
-                self.bullet_list.add(GameSprite(self.player.xcor + (self.player.rect.width/2 - 5), self.player.ycor, 10, 24, self.currentBullet, 0, self.bulletSpeed))
+                self.bulletTimer = self.player.fireRate
+                self.shoot_bullet()
 
     def move_enemy_ship_down(self, enemyShip):
-        enemyShip.ycor += enemyShip.yspeed
+        enemyShip.gameSprite.ycor += enemyShip.speed
+
+    def shoot_bullet(self):
+        bullet = Bullet(self.player.gameSprite.xcor + (self.player.gameSprite.rect.width / 2 - 5), self.player.gameSprite.ycor, self.player.currentBullet[2], self.player.currentBullet[3], self.player.currentBullet[4], self.player.currentBullet[5])
+        self.bulletList.add(bullet.gameSprite)
+        bullet.sound.play()
+
+
+class Player(object):
+    # player class (GameSprite)
+    def __init__(self, game_sprite):
+        self.gameSprite = game_sprite
+        self.fireRate = .5
+        self.playerSpeed = 4
+        self.bulletSpeed = 5
+        self.smallBasicBullet = [game_sprite.xcor + (game_sprite.rect.width / 2 - 5), game_sprite.ycor, 10, 24, [pygame.image.load('Projectiles/Small_Basic_Bullet.png')], pygame.mixer.Sound('Projectiles/Basic_Bullet.wav')]
+        self.currentBullet = self.smallBasicBullet
+
+
+class Bullet(object):
+    # bullet class (xcoordinate, ycoordinate, image width, image height, bullet png, bullet wav)
+    def __init__(self, xcor, ycor, width, height, bullet_sprite, bullet_sound):
+        self.sound = bullet_sound
+        self.gameSprite = GameSprite(xcor, ycor, width, height, bullet_sprite, 0)
+
+
+class Enemy(object):
+    # player class (GameSprite)
+    def __init__(self, game_sprite):
+        self.gameSprite = game_sprite
+        self.speed = 1
+
+
--- View/ParentView.py
+++ View/ParentView.py
@@ -93,7 +93,7 @@ class Sprite(pygame.sprite.Sprite):
 class GameSprite(pygame.sprite.Sprite):
     # class for a Sprite. To create a sprite you must provide: (x coordinate, y coordinate, size of the image, array of
     # all the images, and the starting frame)
-    def __init__(self, xcor, ycor, width, height, images, starting_frame, speed):
+    def __init__(self, xcor, ycor, width, height, images, starting_frame):
         super(GameSprite, self).__init__()
         self.xcor = xcor
         self.ycor = ycor
@@ -106,8 +106,6 @@ class GameSprite(pygame.sprite.Sprite):
         self.index = starting_frame
         # 'image' is the current image of the animation.
         self.image = images[self.index]
-        self.xspeed = speed
-        self.yspeed = speed
 
     def update_time_dependent(self, screen, dt):
         # Updates the image of Sprite based on animation_time. Must provide: (the window, milliseconds since last frame)
@@ -123,36 +121,3 @@ class GameSprite(pygame.sprite.Sprite):
         # (the window, milliseconds since last frame)
         self.update_time_dependent(screen, dt)
 
-
-class EnemyShip(pygame.sprite.Sprite):
-    # class for a Sprite. To create a sprite you must provide: (x coordinate, y coordinate, size of the image, array of
-    # all the images, and the starting frame)
-    def __init__(self, xcor, ycor, size, images, starting_frame):
-        super(EnemyShip, self).__init__()
-        self.xcor = xcor
-        self.ycor = ycor
-        # creates rectangle for the sprite
-        self.rect = pygame.Rect((xcor, ycor), (size, size))
-        self.images = images
-        # time it takes for the the sprite moves to the next frame
-        self.animationTime = .08
-        self.currentTime = 0
-        self.index = starting_frame
-        # 'image' is the current image of the animation.
-        self.image = images[self.index]
-        self.xspeed = 1
-        self.yspeed = 1
-
-    def update_time_dependent(self, screen, dt):
-        # Updates the image of Sprite based on animation_time. Must provide: (the window, milliseconds since last frame)
-        self.currentTime += dt
-        if self.currentTime >= self.animationTime:
-            self.currentTime = 0
-            self.index = (self.index + 1) % len(self.images)
-            self.image = self.images[self.index]
-        screen.blit(self.image, (self.xcor, self.ycor))
-
-    def update(self, screen, dt):
-        # This is the method that's being called when 'all_sprites.update(dt)' is called. Must provide:
-        # (the window, milliseconds since last frame)
-        self.update_time_dependent(screen, dt)
